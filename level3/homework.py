2721d396f3b62de47459ec18609cbd096ddf6c85
demeamzaifarashvili <demeazma6@gmail.com>
2024-05-21 17:31:38 +0400

    second commit

---
+++ level3/homework.py
@@ -0,0 +1,11 @@
+print(5+5) #მიმატება
+print(5-5) #გამოკლება
+print(5*5) #გამრავლება
+print(5/5) #გაყოფა
+
+number = 7 #აქ შევქმენი ცვლადი მივანიჭე ციფრის მნიშვნელობა
+number1 = 4
+print(number + number1) #გავაკეთე ყველა არითმეტიკური ოპერაცია
+print(number / number1) #ბრჭყალები რადგან ცვლადია
+print(number * number1)
+print(number - number1)
