52d2c34b06f0be798d696db9eb92491aaa4bff96
demeamzaifarashvili <demeazma6@gmail.com>
2024-05-28 14:07:42 +0400

    second commit

---
+++ level5/homework.py
@@ -0,0 +1,27 @@
+name = "deme"
+surname = "azmaifarashvili"
+age = 16
+adress = "digmis_masivi"
+
+print(name)
+print(surname)
+print(age)
+print(adress)
+
+number1 = 2      #აქ შევქმენი 10 ცვლადი მივანიჭე ციფრის მნიშვნელობა
+number2 = 6
+number3 = 4
+number4 = 3
+number5 = 9
+number6 = 8
+number7 = 1
+number8 = 7
+number9 = 2
+number10 = 1
+
+#დავიწყე მათემატიკური ოპერაციების გაკეტება
+
+print(number1 + number2)   #მიმატება
+print(number3 * number6)   #გამრავლება
+print(number5 - number8)   #გამოკლება
+print(number5 / number4)   #გაყოფა
