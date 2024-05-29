b23fa2fe3d5a38bd272c475bb39957bfd1f3baab
demeamzaifarashvili <demeazma6@gmail.com>
2024-05-29 20:25:45 +0400

    second commit

---
+++ level6/homework.py
@@ -0,0 +1,26 @@
+# პირველი დავალება მომხმარებელს შემოვატანინე ორი
+# რივხვი და გავაკეთე ყველა მათემატიკური ოპერაცია
+number = input()
+number1 = input()
+print(int(number) + int(number1))
+print(int(number) - int(number1))
+print(int(number) * int(number1))
+print(int(number) / int(number1))
+# შევქმენი მარტივი  პროგრამა სადაც მომხმარებელი
+# დაწერს თავის სახელს და მისალოც ტექსტს
+name = input("please enter your name  :")
+birthday = input("please enter birthday text  :")
+
+# შევქმენი მომხმარებლისთვის სარეგისტრაციო სამი ცვლადი
+username = input("please enter your username  :")
+lastname = input("please enter your lastname  :")
+age = input("please enter your age  :")
+print(username)
+print(lastname)
+print(age)
+#დავპრინტე ყყველაფერი ერთად 
+# აქ გამოვთვალე ჩემი ოჯახის წევრების ასაკი 25 წლის შემდეგ
+mom = input()
+print(int(mom) + 25)
+dad = input()
+print(int(dad) + 25)
