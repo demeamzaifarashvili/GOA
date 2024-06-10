048b2abe51c32331976675a382f4c7736afc23af
demeamzaifarashvili <demeazma6@gmail.com>
2024-06-10 23:00:39 +0400

    second commit

--- claswork2/claswork.py
+++
@@ -1,5 +0,0 @@
-firstname = input("please enter your name : ")
-lastname = input("please enter your lastname : ")
-age = input ("please enter your age : ")
-
-print("saxeli : {name} gvari : {lastname} asaki : {age}") 
