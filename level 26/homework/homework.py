2b785a3a471ff8b7bf76d60741ec35aea3826bd3
demeamzaifarashvili <demeazma6@gmail.com>
2024-08-10 17:39:36 +0400

    commit

---
+++ level 26/homework/homework.py
@@ -0,0 +1,39 @@
+name = "dEMeTRE"  #didi asoebit wers
+print(name.upper())
+
+user_name = "DEMETRE"  #patara asoebit wers
+print(user_name.lower())
+
+nam = "dEmetre"   #iwyebs didi asoti da agrdzelebs patarebit
+print(nam.capitalize())
+
+
+food = ["apple",  "orange" , "cherry"]
+food.append ("limon") #amatebs siashi append
+
+print(food)
+
+car = ["bmw", "lambo", "mercedes"]
+car.insert(1,"mercedes") #svavs mititebul indexze monacems
+
+print(car)
+
+names = ["gio", "deme", "nika"] #shlis siidan
+names.pop(1)
+
+print(names)
+
+fruit = ["apple","orange"]
+fruit.pop(0)
+
+print(fruit)
+
+film = ["batman", "spiderman"]
+film.append("iron_man")
+
+print(film)
+
+programing = ["python", "html", "css"]
+programing.insert(2,"java")
+
+print(programing)
