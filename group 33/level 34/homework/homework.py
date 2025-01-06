9cabf0bdef7e233f93476f2faaf3b5f610137147
demeamzaifarashvili <demeazma6@gmail.com>
2025-01-06 14:50:16 +0400

    commit

--- group 33/level 34/homework/homework.py
+++ group 33/level 34/homework/homework.py
@@ -1,1 +1,21 @@
-#soon 
+def rental_car_cost(d):
+    cost_day = 40
+    total_cost = d * cost_day
+
+    if d >= 7:
+        total_cost -= 50
+
+
+def quarter(month):
+    if month in [1, 2, 3]:
+        return 1
+    elif month in [6, 5, 6]:
+        return 2
+    elif month in [9, 8, 9]:
+        return 3
+    elif month in [11, 12, 12]:
+        return 4
+    
+
+def remove_exclamation_marks(s):
+    return s.replace( )
