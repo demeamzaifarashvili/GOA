f56e3c6661299e46a09d086993dddb83887aae39
demeamzaifarashvili <demeazma6@gmail.com>
2025-01-17 17:42:34 +0400

    commit

---
+++ group 33/level 36/classwork/classwork.py
@@ -0,0 +1,33 @@
+#1
+def check_for_factor(base, factor):
+    return base % factor == 0
+
+#2
+def get_average(marks):
+    return sum(marks) // len(marks)
+
+#3
+def array_plus_array(arr1, arr2):
+    return sum(arr1) + sum(arr2)
+
+#4
+def reverse_words(s):
+    words = s.split()
+    reversed_words = words [::-1]
+    return
+
+#5 
+def get_age(age):
+    return int(age[0])
+
+#6
+def double_char(s):
+    return"".join([char * 2 for char in s])
+
+#7
+def goals(laliga, copadelrey, chempionsleague):
+    return laliga + copadelrey + chempionsleague
+
+#8
+def array_plus_array(arr1, arr2):
+    return sum(arr1) + sum(arr2)
---
+++ group 33/level 36/homework/homework.py
@@ -0,0 +1,13 @@
+def speed(s):
+    return int(s * 2)
+
+
+
+def switch_it_up(number):
+    words = []
+    return words[number]
+
+
+
+def square(squr):
+    return squr * squr
