6d4105dd5dcbfa904d2e75ae7769f0a9d2ede799
demeamzaifarashvili <demeazma6@gmail.com>
2025-02-01 14:53:24 +0400

    commit

---
+++ group 33/level 38/homework/homework.py
@@ -0,0 +1,5 @@
+#1) https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/python
+#2) https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/python
+#3) https://www.codewars.com/kata/56efc695740d30f963000557/train/python
+#4) https://www.codewars.com/kata/577bd026df78c19bca0002c0/train/python
+#5) https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/python
