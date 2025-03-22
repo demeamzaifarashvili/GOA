115924b34241c0450950011ab5f88bf463ec1968
demeamzaifarashvili <demeazma6@gmail.com>
2025-03-22 13:17:02 +0400

    commit

---
+++ group 33/level 45/homework/homework.js
@@ -0,0 +1,18 @@
+let array1 = [22, 55, 11];
+let array2 = ['deme', 'goa', 'demetre'];
+let array3 = [ a, b, c ];
+
+for (let i = 0; i < array1.length; i++) {
+    console.log(array1[i]);
+}
+
+for (let i = 0; i < array2.length; i++) {
+    console.log(array2[i]);
+}
+
+for (let i = 0; i < array3.length; i++) {
+    console.log(array3[i]);
+}
+
+//Java კონსტრუქტორები ან კონსტრუქტორები ჯავაში არის ტერმინოლოგია, რომელიც გამოიყენება ჩვენს პროგრამებში რაღაცის შესაქმნელად. Java-ში კონსტრუქტორი არის სპეციალური მეთოდი, რომელიც გამოიყენება ობიექტების ინიციალიზაციისთვის.
+// ანუ ასე ვთქვათ construction არის  ძირითადი ჩანახაზივით 
